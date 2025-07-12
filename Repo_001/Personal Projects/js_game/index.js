5f5dd49a0652aae151e795e61984c32fcbc9840c
Nasif <md.nasifiqbal006@gmail.com>
2025-07-12 22:55:00 +0600

    class sprite initiated

--- Repo_001/Personal Projects/js_game/index.js
+++ Repo_001/Personal Projects/js_game/index.js
@@ -5,3 +5,23 @@ canvas.width = 1024
 canvas.height = 576
 
 c.fillRect (0, 0, canvas.width, canvas.height)
+
+class sprite {
+    constructor(position){
+        this.position= position
+    }
+
+    draw (){
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+}
+
+const player = new sprite ({
+    x : 0,
+    y : 0
+})
+
+player.draw()
+
+console.log(player);
