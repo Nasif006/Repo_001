11a2a5885d8fd55a05a203ad0df3aa6594f35167
Nasif <md.nasifiqbal006@gmail.com>
2025-07-13 23:16:09 +0600

    color is dynamic

--- Repo_001/Personal Projects/js_game/index.js
+++ Repo_001/Personal Projects/js_game/index.js
@@ -7,21 +7,28 @@ canvas.height = 576
 c.fillRect (0, 0, canvas.width, canvas.height)
 
 class sprite {
-    constructor(position){
+    constructor({position, velocity, color}){
         this.position= position
+        this.velocity= velocity
+        this.color= color
     }
 
     draw (){
-        c.fillStyle = 'red'
+        c.fillStyle = this.color
         c.fillRect(this.position.x, this.position.y, 50, 150)
     }
 }
 
 const player = new sprite ({
-    x : 0,
-    y : 0
-})
+    position : {x : 100, y : 0},
+    color : 'green'})
+
+const enemy = new sprite ({
+    position : {x : 900, y : 0},
+    color : 'red'})
 
 player.draw()
+
+enemy.draw()
 
 console.log(player);
